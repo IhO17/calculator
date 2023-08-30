e8e43393df17004a532b212dcfc7b6870d6e3f05
IhO17 <107650639+IhO17@users.noreply.github.com>
2023-08-30 22:33:37 +0200

    operate()

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
